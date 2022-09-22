9e84f2f11f87450d956d88a64cc1772813523811
Nikita Kotenko <nikita_kot2@mail.ru>
2022-09-22 14:38:42 +0300

    packages: Uninstall react-buddy packages and plugin

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import React, { lazy } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { DevSupport } from "@react-buddy/ide-toolbox";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 const App = lazy(() => import("./pages/App"));
-import { ComponentPreviews, useInitial } from "./dev";
 
 import "./index.scss";
 import "ol/ol.css";
@@ -20,12 +18,10 @@ const darkTheme = createTheme({
 });
 
 root.render(
-  <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </DevSupport>
+  <BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
