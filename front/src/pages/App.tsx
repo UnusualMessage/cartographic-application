e373dd1c86e93db93b94a99039aca6599e58946d
Nikita Kotenko <nikita_kot2@mail.ru>
2022-10-02 19:34:28 +0300

    Split App into Header and Main content

--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,53 +1,13 @@
 import { observer } from "mobx-react-lite";
 
-import Map from "../components/Map";
-import Controls from "../components/Map/Controls";
-import { TileLayer, VectorLayer } from "../components/Map/Layer";
-import View from "../components/Map/View";
-import Interactions from "../components/Map/Interactions";
-import Overlay from "../components/Map/Overlay";
-import ContextMenu from "../components/Map/ContextMenu";
-import InteractionsStore from "../stores/InteractionsStore";
-import TableTabs from "../components/Tabs/TableTabs";
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 const App = () => {
-  const readonly = InteractionsStore.readonly;
-
-  const contextMenu = (
-    <div>
-      <ContextMenu />
-    </div>
-  );
-
-  let overlay = <></>;
-
-  if (readonly) {
-    overlay = (
-      <div>
-        <Overlay />
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
-
-      <Map>
-        <Controls />
-        <View />
-        <TileLayer name={"base"} />
-
-        <VectorLayer name={"draw"}>
-          {readonly ? <></> : <Interactions />}
-        </VectorLayer>
-
-        {contextMenu}
-        {overlay}
-      </Map>
-
-      <TableTabs />
+      <Main />
     </>
   );
 };
