4057bbfb5c5571dfe2f9588929dcfe36e5ee67fb
UnusualMessage <nikita_kot2@mail.ru>
2022-11-08 11:14:21 +0300

    feature: Create Caching Service

--- back/Identity.Application/Handlers/Query/User/GetAccessTokenHandler.cs
+++ back/Identity.Application/Handlers/Query/User/GetAccessTokenHandler.cs
@@ -1,51 +1,26 @@
-﻿using System.Text;
-using Common.Core.Exceptions;
+﻿using Common.Core.Exceptions;
 using Identity.Application.Requests.Queries.User;
 using Identity.Application.Responses.User;
-using Identity.Core.Interfaces.Repositories;
 using Identity.Core.Interfaces.Services;
 using MediatR;
-using Microsoft.Extensions.Caching.Distributed;
-using Newtonsoft.Json;
 
 namespace Identity.Application.Handlers.Query.User;
 
 public class GetAccessTokenHandler : IRequestHandler<GetAccessToken, AuthenticateUserResponse>
 {
-    private readonly IUserRepository _userRepository;
     private readonly ITokenService _tokenService;
-    private readonly IDistributedCache _cache;
+    private readonly ICachingService<Core.Entities.User> _cachingService;
 
-    public GetAccessTokenHandler(IUserRepository userRepository, ITokenService tokenService, IDistributedCache cache)
+    public GetAccessTokenHandler(ITokenService tokenService, ICachingService<Core.Entities.User> cachingService)
     {
-        _userRepository = userRepository;
         _tokenService = tokenService;
-        _cache = cache;
+        _cachingService = cachingService;
     }
     
     public async Task<AuthenticateUserResponse> Handle(GetAccessToken request, CancellationToken cancellationToken)
     {
         var cacheKey = request.RefreshToken;
-        Core.Entities.User? user;
-
-        var cachedUser = await _cache.GetAsync(cacheKey, cancellationToken);
-
-        if (cachedUser is not null)
-        {
-            var serializedUser = Encoding.UTF8.GetString(cachedUser);
-            user = JsonConvert.DeserializeObject<Core.Entities.User>(serializedUser);
-        }
-        else
-        {
-            user = await _userRepository.GetUserByTokenAsync(request.RefreshToken ?? "");
-            var serializedUser = JsonConvert.SerializeObject(user);
-            cachedUser = Encoding.UTF8.GetBytes(serializedUser);
-            
-            await _cache.SetAsync(cacheKey, cachedUser,
-                new DistributedCacheEntryOptions()
-                    .SetSlidingExpiration(TimeSpan.FromMinutes(5))
-                    .SetAbsoluteExpiration(DateTime.Now.AddHours(6)), cancellationToken);
-        }
+        var user = await _cachingService.Cache(cacheKey ?? "");
 
         if (user is null)
         {
