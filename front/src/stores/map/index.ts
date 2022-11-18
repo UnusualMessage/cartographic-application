636ffec05ec70e3efe63c5198e43cbf1735d5588
Nikita Kotenko <nikita_kot2@mail.ru>
2022-11-18 19:14:13 +0300

    refactor: Refactor Features Modify Injectors

--- front/src/stores/map/index.ts
+++ front/src/stores/map/index.ts
@@ -2,7 +2,6 @@ import InteractionsStore from "./InteractionsStore";
 import LayersStore from "./LayersStore";
 import MapStore from "./MapStore";
 import OverlaysStore from "./OverlaysStore";
-import StylesStore from "./StylesStore";
 import ViewStore from "./ViewStore";
 import FeaturesStore from "./FeaturesStore";
 
@@ -11,7 +10,6 @@ export {
   LayersStore,
   MapStore,
   OverlaysStore,
-  StylesStore,
   ViewStore,
   FeaturesStore,
 };
