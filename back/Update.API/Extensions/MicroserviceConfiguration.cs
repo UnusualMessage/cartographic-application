a7a8dd24148f1e3a79a0ed7e0cde5a7bb4379434
Nikita Kotenko <nikita_kot2@mail.ru>
2022-11-18 11:57:11 +0300

    feaure: Init Update service

---
+++ back/Update.API/Extensions/MicroserviceConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Update.API.Extensions;
+
+public static class MicroserviceConfiguration
+{
+    public static void AddMicroservice(this IServiceCollection services)
+    {
+    }
+}
---
+++ back/Update.API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Update.API.Extensions;
+
+namespace Update.API;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    private IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors();
+        services.AddMicroservice();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseCors(builder => builder
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+            );
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => { });
+    }
+}
