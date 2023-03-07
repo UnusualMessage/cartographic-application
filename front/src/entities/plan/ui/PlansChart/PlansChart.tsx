e19bd70a5781bd716f2ab1c5f5ee314ae5f46c67
Nikita Kotenko <nikita_kot2@mail.ru>
2023-03-07 15:08:28 +0300

    packages: Install ant-design/plots package

--- front/src/entities/plan/ui/PlansChart/PlansChart.tsx
+++ front/src/entities/plan/ui/PlansChart/PlansChart.tsx
@@ -1,4 +1,4 @@
-import { ResponsivePie } from "@nivo/pie";
+import { Pie } from "@ant-design/plots";
 
 import type { Plan } from "@shared/misc";
 import { chart, main } from "@shared/styles";
@@ -7,30 +7,34 @@ interface Props {
   plans: Plan[];
 }
 
-interface ChartData {
-  id: string;
-  value: number;
-  color: string;
-}
-
 const PlansChart = ({ plans }: Props) => {
   const data = plans.map((plan) => {
     return {
-      id: `${plan.type}-${plan.year}`,
+      title: `${plan.type}-${plan.year}`,
       value: plan.target,
-      color: "hsl(81, 70%, 50%)",
     };
   });
+
+  const config = {
+    appendPadding: 10,
+    data: data,
+    angleField: "value",
+    colorField: "title",
+    radius: 0.8,
+    label: {
+      type: "outer",
+    },
+    interactions: [
+      {
+        type: "element-active",
+      },
+    ],
+  };
 
   return (
     <div className={chart}>
       <div className={main}>
-        <ResponsivePie<ChartData>
-          data={data}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          startAngle={-180}
-          enableArcLabels={false}
-        />
+        <Pie {...config} />
       </div>
     </div>
   );
