c905f26a8dff687b5ee976f35e00e0804e45167e
Nikita Kotenko <nikita_kot2@mail.ru>
2023-04-13 22:38:40 +0300

    feature: Add empty slides

---
+++ front/src/features/landing/ui/ExamplesSlide/ExamplesSlide.tsx
@@ -0,0 +1,5 @@
+const ExamplesSlide = () => {
+  return <div></div>;
+};
+
+export default ExamplesSlide;
---
+++ front/src/features/landing/ui/OrderSlide/OrderSlide.tsx
@@ -0,0 +1,5 @@
+const OrderSlide = () => {
+  return <div></div>;
+};
+
+export default OrderSlide;
